3bf17b02188af4c62f4f2a5f76c978ebc2ded910
hahachapury <lamaalana17@gmail.com>
2025-10-16 20:07:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-'''
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Дмитрий')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-'''
 RED = '\u001b[41m'
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
